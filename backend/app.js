3b30b5890a9a852915eba8c9c48c982c457a7c91
yogesh <103855728+yogesh4343@users.noreply.github.com>
2024-02-08 05:04:36 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({extended: true}))
 
 app.use(cors(
     {
-        origin : ["https://deploy-mern-1whq.vercel.app"],
+        origin : [""],
         methods :["POST" , "GET"],
         credentials : true
     }
