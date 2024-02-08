300147da574ac7e3a79be4a4a863f9b0079c2d65
yogesh <103855728+yogesh4343@users.noreply.github.com>
2024-02-08 07:21:54 +0530

    Update UserRoutes.js

--- backend/Routes/UserRoutes.js
+++ backend/Routes/UserRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { RegisterUser , Login, AlreadyLogin , AllUser} = require("../Controller/UserController");
+const { RegisterUser , Login, AlreadyLogin , AllUser , SignOut  } = require("../Controller/UserController");
 const { Authenticate } = require("../Utils/Authenticate");
 
 const Router = express.Router();
@@ -9,6 +9,7 @@ Router.route("/register").post(RegisterUser);
 Router.route("/login").post(Login); 
 Router.route("/me").get(Authenticate , AlreadyLogin); 
 Router.route("/users").get(  AllUser); 
+Router.route("/logout").get(   SignOut); 
 
 
 module.exports = Router
