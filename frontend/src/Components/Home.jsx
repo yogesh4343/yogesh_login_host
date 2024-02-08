782ffcaca5643ae473a421d20813c700a21ee10e
yogesh <103855728+yogesh4343@users.noreply.github.com>
2024-02-08 07:19:12 +0530

    Update Home.jsx

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom';
+import { LogOutReducer } from '../Store/Slice/UserSlice';
 
 const Home = () => {
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const signOutFunc = ()=>{
+    dispatch(LogOutReducer());
+    console.log("signOut")
+
+    navigate('/login')
+
+  }
+
+
   return (
     <div>
  <h2 style={{color: "rgb(51 79 135)"}}  className="my-3 display-3 fw-bold ls-tight"  >Ho</h2>
+ <br /> <br /> <br /> <br /> 
+ <button onClick={signOutFunc}>Sign Out </button>        
     </div>
   )
 }
